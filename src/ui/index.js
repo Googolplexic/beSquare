a51f655b04d3bb1757bb775a624ad3792a35c0d8
Googolplexic <colemancflai@yahoo.com>
2024-10-05 13:38:11 -0700

    Uh now it works I guess

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,29 +2,19 @@ import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
 addOnUISdk.ready.then(async () => {
     console.log("addOnUISdk is ready for use.");
-
     // Get the UI runtime.
     const { runtime } = addOnUISdk.instance;
-
     // Get the proxy object, which is required
-    // to call the APIs defined in the Document Sandbox runtime
-    // i.e., in the `code.js` file of this add-on.
-    const sandboxProxy = await runtime.apiProxy("documentSandbox");
-
+    // to call the APIs defined in the Script runtime
+    // i.e., in the code.js file of this add-on.
+    const scriptApi = await runtime.apiProxy("script");
     const createRectangleButton = document.getElementById("createRectangle");
     createRectangleButton.addEventListener("click", async event => {
-        await sandboxProxy.createRectangle();
+        await scriptApi.createRectangle();
     });
-
-    const createSquareButton = document.getElementById("createSquare");
-    createSquareButton.addEventListener("click", async event => {
-        await sandboxProxy.createRectangle({ width: 100, height: 100 });
-    });
-
-    // Enable the buttons only when:
-    // 1. `addOnUISdk` is ready,
-    // 2. `sandboxProxy` is available, and
-    // 3. `click` event listeners are registered.
+    // Enable the button only when:
+    // 1. addOnUISdk is ready,
+    // 2. scriptApi is available, and
+    // 3. click event listener is registered.
     createRectangleButton.disabled = false;
-    createSquareButton.disabled = false;
 });
