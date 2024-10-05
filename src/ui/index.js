56fd4592b366168a52766294f1e8a1c59602b106
Googolplexic <colemancflai@yahoo.com>
2024-10-05 12:53:05 -0700

    test

---
+++ src/ui/index.js
@@ -0,0 +1,31 @@
+import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+
+
+addOnUISdk.ready.then(async () => {
+    console.log("addOnUISdk is ready for use.");
+
+    // Get the UI runtime.
+    const { runtime } = addOnUISdk.instance;
+
+    // Get the proxy object, which is required
+    // to call the APIs defined in the Document Sandbox runtime
+    // i.e., in the `code.js` file of this add-on.
+    const sandboxProxy = await runtime.apiProxy("documentSandbox");
+
+    const createRectangleButton = document.getElementById("createRectangle");
+    createRectangleButton.addEventListener("click", async event => {
+        await sandboxProxy.createRectangle();
+    });
+
+    const createSquareButton = document.getElementById("createSquare");
+    createSquareButton.addEventListener("click", async event => {
+        await sandboxProxy.createRectangle({ width: 100, height: 100 });
+    });
+
+    // Enable the buttons only when:
+    // 1. `addOnUISdk` is ready,
+    // 2. `sandboxProxy` is available, and
+    // 3. `click` event listeners are registered.
+    createRectangleButton.disabled = false;
+    createSquareButton.disabled = false;
+});
